f52d08fa7a21ec3a0e1c1ea4beccb1196a631c20
Jonatas Craveiro <jonatascraveiro@gmail.com>
2022-06-24 00:07:39 -0400

    feat inserido context para atualizar o nome de usuario

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, { BaseSyntheticEvent, useState } from 'react'
 import { Button, Col, Form, Row } from 'react-bootstrap'
+import useGithub from '../../hooks/github-hooks'
 
 import { Container } from './styles'
 
 const Header: React.FC = () => {
+  const { getUsuario } = useGithub()
+  const [nomeUsuario, setNomeUsusario] = useState('jonatascraveiro')
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    getUsuario(nomeUsuario)
+  }
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Row className="align-items-center py-3 m-auto">
           <Col className="col-11">
             <Form.Group className="mb-3">
               <Form.Control
                 type="text"
                 placeholder="Informe o nome do usuario"
+                onChange={(e: BaseSyntheticEvent) => {
+                  setNomeUsusario(e.target.value)
+                }}
               />
             </Form.Group>
           </Col>
           <Col className="col-1">
             <Form.Group className="mb-3">
-              <Button variant="primary" className="rounded-3" type="submit">
+              <Button
+                variant="primary"
+                disabled={nomeUsuario.trim().length == 0}
+                className="rounded-3"
+                type="submit"
+              >
                 Buscar
               </Button>
             </Form.Group>
