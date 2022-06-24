7db7ae0bb0ca2ab74a37c12384324e56733efe69
Jonatas Craveiro <jonatascraveiro@gmail.com>
2022-06-24 00:06:52 -0400

    feat inserido dados via context

--- src/Components/Biografia/index.tsx
+++ src/Components/Biografia/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Col, Image, Row, Stack } from 'react-bootstrap'
+import useGithub from '../../hooks/github-hooks'
 
 import { Container } from './styles'
 
 const Biografia: React.FC = () => {
+  const { githubState } = useGithub()
+  const { usuario } = githubState
   return (
     <Container>
       <Row className="justify-content-start">
@@ -12,37 +15,38 @@ const Biografia: React.FC = () => {
             roundedCircle
             width="90%"
             alt="Foto do perfil"
-            src="https://avatars.githubusercontent.com/u/29283919?v=4"
+            src={usuario?.avatar_url}
           />
         </Col>
         <Col xs={12} md={3}>
           <Stack gap={4} className=" mx-auto">
-            <h1>Jonatas Craveiro</h1>
+            <h1 className="text-capitalize">{usuario?.name}</h1>
             <div>
-              <b>Nome de usuário: </b> jonatascraveiro
+              <b>Nome de usuário: </b> {usuario?.name}
             </div>
             <div>
-              <b>Empresa: </b> Empresa do Jonatas
+              <b>Empresa: </b>
+              {usuario?.company ?? 'Não possui empresa'}
             </div>
             <div>
-              <b>Localização: </b> Brasil
+              <b>Localização: </b> {usuario?.location ?? 'Não informado'}
             </div>
             <Row className="text-center">
               <Col>
                 <h5>Seguidores</h5>
-                <p>38</p>
+                <p>{usuario?.followers}</p>
               </Col>
               <Col>
                 <h5>Seguindo</h5>
-                <p>38</p>
+                <p>{usuario?.following}</p>
               </Col>
               <Col>
                 <h5>Gist</h5>
-                <p>38</p>
+                <p>{usuario?.public_gists}</p>
               </Col>
               <Col>
                 <h5>Repositórios</h5>
-                <p>38</p>
+                <p>{usuario?.public_repos}</p>
               </Col>
             </Row>
           </Stack>
