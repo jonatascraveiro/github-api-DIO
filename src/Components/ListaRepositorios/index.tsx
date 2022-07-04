7cd12fd31a165949203110fab56a5c73034679a9
Jonatas Craveiro <jonatascraveiro@gmail.com>
2022-07-03 21:44:57 -0400

    feat inserido compoente item a lista de repositorios

--- src/Components/ListaRepositorios/index.tsx
+++ src/Components/ListaRepositorios/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Row } from 'react-bootstrap'
+import { RepositoriosGithubType } from '../../types/repositorios'
 import ItemLista from '../ItemLista'
 
 import { Container } from './styles'
 
-const ListaRepositorios: React.FC = () => {
+type ListaRepositoriosType = {
+  repositorios: RepositoriosGithubType[]
+}
+const ListaRepositorios: React.FC<ListaRepositoriosType> = ({
+  repositorios,
+}) => {
   return (
     <Container>
       <Row>
-        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((key) => {
-          return <ItemLista key={key} />
+        {repositorios.map((repositorio) => {
+          return <ItemLista item={repositorio} key={repositorio.id} />
         })}
       </Row>
     </Container>
