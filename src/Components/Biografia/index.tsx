fe0cdb2daf9fd13134d6af7a0c77fc542eab51c6
Jonatas Craveiro <jonatascraveiro@gmail.com>
2022-07-04 23:40:44 -0400

    alterado responsividade

--- src/Components/Biografia/index.tsx
+++ src/Components/Biografia/index.tsx
@@ -10,15 +10,16 @@ const Biografia: React.FC = () => {
   return (
     <Container>
       <Row className="justify-content-start">
-        <Col xs={12} md={2}>
+        <Col xs={12} sm={12} md={3} lg={2} xl={2}>
           <Image
+            style={{ maxWidth: '350px' }}
             roundedCircle
             width="90%"
             alt="Foto do perfil"
             src={usuario?.avatar_url}
           />
         </Col>
-        <Col xs={12} md={3}>
+        <Col xs={12} sm={12} md={4} lg={6}>
           <Stack gap={4} className=" mx-auto">
             <h1 className="text-capitalize">{usuario?.name}</h1>
             <div>
@@ -31,20 +32,20 @@ const Biografia: React.FC = () => {
             <div>
               <b>Localização: </b> {usuario?.location ?? 'Não informado'}
             </div>
-            <Row className="text-center">
-              <Col>
+            <Row className="text-md-center text-sm-start text-start">
+              <Col xs={12} sm={6} md={6} lg={3}>
                 <h5>Seguidores</h5>
                 <p>{usuario?.followers}</p>
               </Col>
-              <Col>
+              <Col xs={12} sm={6} md={6} lg={3}>
                 <h5>Seguindo</h5>
                 <p>{usuario?.following}</p>
               </Col>
-              <Col>
+              <Col xs={12} sm={6} md={6} lg={3}>
                 <h5>Gist</h5>
                 <p>{usuario?.public_gists}</p>
               </Col>
-              <Col>
+              <Col xs={12} sm={6} md={6} lg={3}>
                 <h5>Repositórios</h5>
                 <p>{usuario?.public_repos}</p>
               </Col>
